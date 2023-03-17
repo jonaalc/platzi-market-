ee2a33747e800656baa7f9ba47654f35d05c98fc
Jonathan Perez <jonaalc@gmail.com>
2023-03-17 00:05:31 -0300

    Creación de los primeros Query Methods

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,12 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entities.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 //se le pasa la clase y el tipo de datos de la pk
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
